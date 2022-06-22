bd4125c8d2c9ca0c5b7598e786d4dd073d0659cb
teemo <280234636@qq.com>
2022-06-22 15:38:21 +0800

    所有功能已经接通并实现的的版本。

---
+++ mpmarket/src/main/java/com/mpprojects/mpmarket/config/SwaggerConfig.java
@@ -0,0 +1,11 @@
+package com.mpprojects.mpmarket.config;
+
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig extends WebMvcConfigurationSupport {
+}
