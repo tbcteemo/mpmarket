1b3e79ac19a8af5fc7687d3826335737920a4703
Teemo <280234636@qq.com>
2022-06-26 17:07:33 +0800

    1.修改了版本号为1.0.2 2.更改了swagger2的版本为2.9.2（原版本3.0.0） 3.将swagger的ui由swagger-ui更改为了swagger-bootstrap-ui

--- mpmarket/src/main/java/com/mpprojects/mpmarket/config/SwaggerConfig.java
+++ mpmarket/src/main/java/com/mpprojects/mpmarket/config/SwaggerConfig.java
@@ -1,8 +1,8 @@
 package com.mpprojects.mpmarket.config;
 
+import com.github.xiaoymin.swaggerbootstrapui.annotations.EnableSwaggerBootstrapUI;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
@@ -12,6 +12,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
+@EnableSwaggerBootstrapUI
 public class SwaggerConfig {
 
     @Bean
